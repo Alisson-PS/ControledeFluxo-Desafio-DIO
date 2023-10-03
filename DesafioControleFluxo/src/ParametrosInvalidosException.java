b49bf9e9fa90d60fd484bf012ebd1285f3dafa42
Alisson-PS <133115778+Alisson-PS@users.noreply.github.com>
2023-10-03 13:56:45 -0300

    Add files via upload

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+
+
+public class ParametrosInvalidosException extends Exception{
+    
+} /* {
+  public ParametrosInvalidosException(){
+
+    }public ParametrosInvalidosException(String message) {
+        super();
+    }
+}*/
